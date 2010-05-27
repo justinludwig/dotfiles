94fa6a93ae077d4e5641d7e2ad1fbed687047044
Justin Ludwig <justinludwig@gmail.com>
2010-05-27 14:59:51 -0700

    added support for displaying dock also nixed menubar from terminal

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Actions.CycleWS
+import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Grid
 import XMonad.Layout.Master
 import XMonad.Prompt
@@ -9,10 +10,16 @@ import XMonad.Util.EZConfig
 
 import qualified XMonad.StackSet as W
 
+-- my preferred teminal settings
+myTerminal = "gnome-terminal --hide-menubar"
+
+-- automatically manage docks (gnome-panel/dzen/xmobar/etc)
+myManageHook = manageDocks
+
 -- each layout is separated by |||
 -- layout 1: grid w/ master (expand/contract by 3/100; master takes up 1/2 of screen)
--- layout 2: standard full
-myLayoutHook = mastered (3/100) (1/2) $ Grid ||| Full
+-- layout 2: standard full (hides dock)
+myLayoutHook = (avoidStruts $ mastered (3/100) (1/2) $ Grid) ||| Full
 
 -- add 11 extra workspaces beyond the default 9 (workspaces can be named anything, not just numbers)
 myWorkspaces = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
@@ -30,6 +37,8 @@ myKeys = [
     ]
 
 main = xmonad $ defaultConfig {
+    terminal = myTerminal,
+    manageHook = myManageHook <+> manageHook defaultConfig,
     layoutHook = myLayoutHook,
     workspaces = myWorkspaces
 } `additionalKeysP` myKeys
