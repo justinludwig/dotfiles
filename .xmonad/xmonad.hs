8713de1e58d931a3596bbcc5f9b818863c11e9c0
Justin Ludwig <justinludwig@gmail.com>
2011-06-13 18:44:42 -0700

    added fix for prompt ctrl-C, more workspaces to xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,3 +1,4 @@
+import Data.Map as M
 import XMonad
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.ManageDocks
@@ -21,25 +22,29 @@ myManageHook = manageDocks
 -- layout 2: standard full (hides dock)
 myLayoutHook = (avoidStruts $ mastered (3/100) (1/2) $ Grid) ||| Full
 
--- add 11 extra workspaces beyond the default 9 (workspaces can be named anything, not just numbers)
-myWorkspaces = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
+-- add 26 extra workspaces beyond the default 9
+myWorkspaces = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 -- add keybindings (via EZConfig) to access additional workspaces
 -- M = mod (alt), S = shift
-myKeys = [
+myKeysToAdd = [
     ("M-<Left>", prevWS),
     ("M-<Right>", nextWS),
     ("M-S-<Left>", shiftToPrev),
     ("M-S-<Right>", shiftToNext),
-    ("M-;", workspacePrompt defaultXPConfig (windows . W.view)),
-    ("M-S-;", workspacePrompt defaultXPConfig (windows . W.shift)),
-    ("M-S-p", shellPrompt defaultXPConfig)
+    -- extra promptKeymap business to fix http://code.google.com/p/xmonad/issues/detail?id=317
+    ("M-;", workspacePrompt defaultXPConfig { promptKeymap = M.fromList [((controlMask, xK_c), quit)] `M.union` promptKeymap defaultXPConfig } (windows . W.view)),
+    ("M-S-;", workspacePrompt defaultXPConfig  { promptKeymap = M.fromList [((controlMask, xK_c), quit)] `M.union` promptKeymap defaultXPConfig }(windows . W.shift)),
+    ("M-S-p", shellPrompt defaultXPConfig { promptKeymap = M.fromList [((controlMask, xK_c), quit)] `M.union` promptKeymap defaultXPConfig })
     ]
+
+-- remove some keybindings that I only use accidentally
+myKeysToRemove = ["M-q", "M-S-q"]
 
 main = xmonad $ defaultConfig {
     terminal = myTerminal,
     manageHook = myManageHook <+> manageHook defaultConfig,
     layoutHook = myLayoutHook,
     workspaces = myWorkspaces
-} `additionalKeysP` myKeys
+} `additionalKeysP` myKeysToAdd `removeKeysP` myKeysToRemove
 
